a9f6f59de68250b2e794c0f7969a78127a29b002
Егор <sainstrow.pro26@gmail.com>
2021-11-01 16:19:46 +0300

    add test

---
+++ src/test/com/db/edu/proxy/ParserTest.java
@@ -0,0 +1,17 @@
+package test.com.db.edu.proxy;
+
+import main.com.db.edu.parser.MessageParser;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ParserTest {
+
+    @Test
+    public void baseParserTest(){
+        MessageParser parser = new MessageParser();
+        String clientMessage = "/snd hello";
+        clientMessage = parser.parse(clientMessage);
+
+        assertEquals(" hello", clientMessage);
+    }
+}
