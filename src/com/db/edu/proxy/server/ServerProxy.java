9be9dceea6f773743da0347e80b6f649912ea3fb
ulianapink <black.n.pink@yandex.ru>
2021-11-01 10:52:48 +0300

    ServerProxy simple realization

--- src/com/db/edu/proxy/server/ServerProxy.java
+++ src/com/db/edu/proxy/server/ServerProxy.java
@@ -3,22 +3,38 @@ package com.db.edu.proxy.server;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 
 public class ServerProxy {
-    public static void main(String[] args) {
+
+    private final ArrayList<String> messageBuffer;
+
+    public ServerProxy() {
+        messageBuffer = new ArrayList<>();
+    }
+
+    public void main(String[] args) {
         try (final ServerSocket listener = new ServerSocket(9999);
              final Socket connection = listener.accept();
+
              final DataInputStream input = new DataInputStream(
                      new BufferedInputStream(connection.getInputStream()));
+
              final DataOutputStream out = new DataOutputStream(
                      new BufferedOutputStream(connection.getOutputStream()));
         ) {
 
-            final int int1 = input.readInt();
-            final int int2 = input.readInt();
-            final int int3 = input.readInt();
+            final String receivedMessage = input.readUTF();
 
-            out.writeUTF(int1 + " < " + int2 + " < " +int3 + " < " + " <<<<<<<<");
+            if ("/hist".equals(receivedMessage)) {
+                for (String message : messageBuffer) {
+                    out.writeUTF(message);
+                }
+            } else {
+                messageBuffer.add(receivedMessage);
+                out.writeUTF(receivedMessage);
+            }
+
             out.flush();
 
         } catch (IOException e) {
