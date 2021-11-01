8be5f99e34505ae4826a3220c8f47a03337abdf1
darik333 <darik93@yandex.ru>
2021-11-01 11:06:49 +0300

    Message parser

--- src/com/db/edu/parser/MessageParser.java
+++ src/com/db/edu/parser/MessageParser.java
@@ -1,4 +1,12 @@
 package com.db.edu.parser;
 
 public class MessageParser {
+    public String parser(String message) {
+        if (message.startsWith("/snd")) {
+            return message.substring("/snd ".length());
+        } else if ((message.startsWith("/hist"))) {
+            return "/hist";
+        }
+        throw new IllegalArgumentException("Wrong message");
+    }
 }
