b83eb7506d982d1175931aedad48aa624a34f83e
DoolyEvgesha <oparina.dk@phystech.edu>
2021-11-01 10:26:54 +0300

    client proxy

--- src/com/db/edu/proxy/client/ClientProxy.java
+++ src/com/db/edu/proxy/client/ClientProxy.java
@@ -1,5 +1,8 @@
 package com.db.edu.proxy.client;
 
+import java.io.*;
+import java.net.Socket;
+
 public class ClientProxy {
     public static void main(String[] args) {
         try (
@@ -9,13 +12,12 @@ public class ClientProxy {
                 final DataOutputStream out = new DataOutputStream(
                         new BufferedOutputStream(socket.getOutputStream()))
         ) {
-            out.writeInt(1);
-            out.writeInt(0);
-            out.writeInt(-1);
-            out.flush();
-
-            final String read = input.readUTF();
-            System.out.println("\n>>>>> " + read);
+            while (true) {
+                final String message = input.readUTF();
+                //parsing
+                out.writeUTF(message);
+                out.flush();
+            }
 
         } catch (IOException e) {
             e.printStackTrace();
