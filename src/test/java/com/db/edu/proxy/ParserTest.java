503086a7db21913991fe0b1698c58e94b5a28b59
darik333 <darik93@yandex.ru>
2021-11-02 14:46:38 +0300

    Refactored project structure to make Maven Tests working

--- src/test/java/com/db/edu/proxy/ParserTest.java
+++ src/test/java/com/db/edu/proxy/ParserTest.java
@@ -4,14 +4,13 @@ import main.com.db.edu.parser.MessageParser;
 import org.junit.jupiter.api.Test;
 
 import static org.fest.assertions.Assertions.assertThat;
-
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ParserTest {
-    MessageParser parser = new MessageParser();
+    MessageParser parser =  new MessageParser();
     String clientMessage = "";
     private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
     private final PrintStream stream = new PrintStream(outContent);
@@ -31,8 +30,13 @@ public class ParserTest {
                         "12345_12345_12345_12345_12345_" +
                         "12345_12345_12345_12345_12345_" +
                         "12345_12345_12345_12345_12345_!";
-        final IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class, () -> parser.parse(clientMessage));
-        assertThat(thrown).hasMessage("Sorry, but your message is too long, please, use no more than 150 symbols\n");
+        clientMessage = parser.parse(clientMessage);
+
+        assertEquals(" 12345_12345_12345_12345_12345_" +
+                "12345_12345_12345_12345_12345_" +
+                "12345_12345_12345_12345_12345_" +
+                "12345_12345_12345_12345_12345_" +
+                "12345_12345_12345_12345_12345...", clientMessage);
     }
 
     @Test
@@ -47,9 +51,11 @@ public class ParserTest {
     @Test
     public void noCommandBeforeMessageSent() {
         clientMessage = "Hello";
-        final IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class, () -> parser.parse(clientMessage));
-        assertThat(thrown).hasMessage("You sent a message without any command. Please try again with existing commands. Ex: /snd message\n");
+        final IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class,()->parser.parse(clientMessage));
+        assertThat(thrown).hasMessage("Wrong message");
     }
+
+
 
 
 }
