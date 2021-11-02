a9ffd5c844c02bddd89e322f2cfb55c20b4a3cc2
ulianapink <black.n.pink@yandex.ru>
2021-11-02 16:32:17 +0300

    -

--- src/main/com/db/edu/message/MessageKeeper.java
+++ src/main/com/db/edu/message/MessageKeeper.java
@@ -15,7 +15,7 @@ public class MessageKeeper {
     public MessageKeeper(String roomId) {
         this.roomId = roomId;
         this.fileController = new FileController(roomId);
-        this.messageBuffer = new ArrayList<>();
+        this.messageBuffer = fulfillBufferFromFile();
     }
 
     public synchronized void addMessage(StringMessage message) {
